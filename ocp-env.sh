313d2026fec893018ede51c4785b86b35e737023
Mike Croft <backtotheoldhouse@gmail.com>
2020-05-14 17:12:09 +0100

    update gh workflow

--- ocp-env.sh
+++ ocp-env.sh
@@ -1,7 +1,7 @@
 
 # Borrow Swinches' hard work for AMQ with prom JMX exporter
 oc import-image jboss-amq-63 --from=registry.access.redhat.com/jboss-amq-6/amq63-openshift --confirm -n openshift
-oc new-build registry.access.redhat.com/jboss-amq-6/amq63-openshift:latest~https://github.com/welshstew/amq63-prom-sti.git
+oc -n openshift new-build registry.access.redhat.com/jboss-amq-6/amq63-openshift:latest~https://github.com/welshstew/amq63-prom-sti.git
 curl https://raw.githubusercontent.com/welshstew/amq63-prom-sti/master/amq63-basic-prom.json | oc create -f -
 
 oc new-app --template=amq63-basic-prom \
